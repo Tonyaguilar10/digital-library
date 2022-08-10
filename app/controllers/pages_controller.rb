0f22da08a8f3f60ea8b608bcc583f5da29d9cd53
jose aguilar <jose.aguilar.miranda@gmail.com>
2022-08-10 13:40:58 -0600

    home basic view ready

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    phrases = ['“The books that the world calls immoral are books that show the world its own shame.” ―Oscar Wilde',
+      '“Books are a uniquely portable magic.” ―Stephen King', '
+      “... a mind needs books as a sword needs a whetstone, if it is to keep its edge.”
+      ―George R.R. Marti', '“Only the very weak-minded refuse to be influenced by literature and poetry.”
+      ―Cassandra Clare', '
+      “A great book should leave you with many experiences, and slightly exhausted at the end. You live several lives while reading.”
+      ―William Styron']
+
+    @line = phrases[rand(0..4)]
   end
 end
