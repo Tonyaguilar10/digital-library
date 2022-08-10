38d6db40e83d1cc6d1d1ed73ead0ecc672a21855
jose aguilar <jose.aguilar.miranda@gmail.com>
2022-08-10 03:52:08 -0600

    users controller working, users can be created only by librerian

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "pages#home"
+  get 'users/new', to: 'users#new', as: :create_student
+  post 'users', to: 'users#create'
   resources :books do
     resources :reservations, only: [ :new, :create, :update ]
   end
